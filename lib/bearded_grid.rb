89aa37ba248f86abd905a90ae6ef4bdaca2eb760
Brett Bender <brett@bearded.com>
2013-01-31 17:07:54 -0500

    Fixing an error in trying to require compass

--- lib/bearded_grid.rb
+++ lib/bearded_grid.rb
@@ -1,6 +1,5 @@
-require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('bearded', :path => extension_path)
+Compass::Frameworks.register('bearded', :path => extension_path) if defined?(Compass)
 
 module BeardedGrid
   VERSION = "0.0.2"
