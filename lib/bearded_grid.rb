36095dc8a21a920fac1c794c3443a894c8e5fd97
Brett Bender <brett@bearded.com>
2013-01-31 17:09:28 -0500

    Version bump

--- lib/bearded_grid.rb
+++ lib/bearded_grid.rb
@@ -2,6 +2,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('bearded', :path => extension_path) if defined?(Compass)
 
 module BeardedGrid
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
   DATE = "2013-01-30"
 end
